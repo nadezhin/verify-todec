e6d36f3c21f0d1e57069d4fee019aabb085dedc8
nadezhin <Dmitry.Nadezhin@gmail.com>
2018-06-02 22:20:42 +0300

    Working on section 7

--- acl2/section7.lisp
+++ acl2/section7.lisp
@@ -16,17 +16,28 @@
   :rule-classes :linear
   :enable algo1)
 
+(defruled algo1-as-choose-u_i-w_i
+  (mv-let (i dv) (algo1 from v f)
+    (equal dv (choose-u_i-w_i i v f)))
+  :enable algo1)
+
+(defruled choose-u_i-w_i-thm
+  (implies (not (equal (choose-u_i-w_i i v f) (u_i i v)))
+           (equal (choose-u_i-w_i i v f) (w_i i v)))
+  :enable choose-u_i-w_i)
+
 (defrule dv-in-Rv-algo1
   (let ((dv (mv-nth 1 (algo1 from v f))))
     (in-tau-intervalp dv (Rv v f)))
-  :enable algo1)
+  :use algo1-as-choose-u_i-w_i
+  :enable (algo1 choose-u_i-w_i))
 
 (defrule algo1-u_i-or-v_i
   (mv-let (i dv) (algo1 from v f)
     (or (= dv (u_i i v))
         (= dv (w_i i v))))
   :rule-classes ()
-  :enable algo1)
+  :enable (algo1 choose-u_i-w_i-thm))
 
 (defrule has-D-length-algo1
   (mv-let (i dv) (algo1 from v f)
@@ -120,7 +131,7 @@
  :induct (algo1 from v f)
  :enable algo1
  :hints
- (("subgoal *1/1":use
+ (("subgoal *1/2":use
    ((:instance uninteresting-other-than-u_i-w_i
               (d (pos-rational-fix d))
               (v (pos-rational-fix v))
@@ -129,7 +140,7 @@
                (x d)
                (i j)
                (j from))))))
-#|
+
 (defruled has-minimal-D-length-algo1
  (mv-let (i dv) (algo1 from v f)
    (declare (ignore dv))
@@ -142,76 +153,13 @@
              (not (has-D-length d j))))
  :induct (algo1 from v f)
  :enable algo1
- :hints
- (("subgoal *1/1" :use
-   (:instance uninteresting-other-than-u_i-w_i
-              (v (pos-rational-fix v))
-              (i from)))))
-|#
+ :hints (("subgoal *1/2" :use
+          (:instance uninteresting-other-than-u_i-w_i
+                     (v (pos-rational-fix v))
+                     (i from)))))
 #|
 (acl2::with-arith5-help
  (defruled algo1-meets-specs
-   (mv-let (i dv) (algo1 from v f)
-     (implies (and (in-tau-intervalp d (Rv v f))
-                   (has-D-length d j)
-                   (pos-rationalp d)
-                   (pos-rationalp v)
-                   (not (= d dv))
-                   (posp from)
-                   (posp j))
-              (or (< i j)
-                  (and (= i (max from j))
-                       (< (abs (- dv v)) (abs (- d v))))
-                  (and (= i (max from j))
-                       (= (abs (- dv v)) (abs (- d v)))
-                       (evenp (digitn (f dv) (- i) (D)))
-                       (not (evenp (digitn (f d) (- i) (D))))))))
-   :enable (u_i-linear w_i-linear)
-   :use algo1-u_i-or-v_i
-   :cases ((or (in-tau-intervalp (u_i from v) (Rv v f))
-               (in-tau-intervalp (w_i from v) (Rv v f))))
-   :hints
-   (("subgoal 2" :cases ((< j from) (= j from) (> j from)))
-;:expand (algo1 from v f)
-    ("subgoal 2.3" :use
-     ((:instance uninteresting-other-than-u_i-w_i
-                 (i from))
-      (:instance has-D-length-monotone
-                 (x d)
-                 (i j)
-                 (j from))))
-    ("subgoal 2.2" :use
-     (:instance uninteresting-other-than-u_i-w_i
-                (i from)))
-    ("subgoal 2.1"
-    ("subgoal 200.1" :use
-     ((:instance uninteresting-other-than-u_i-w_i
-                 (i from))
-      (:instance has-D-length-monotone
-                 (x d)
-                 (i j)
-                 (j from))))
-   )))
- |#
-
-(acl2::with-arith5-help
- (defrule evenp-digitn-f-w_i
-   (implies (and (integerp from)
-                 (<= 2 from))
-            (equal (evenp (digitn (f (w_i from v)) (- from) (D)))
-                   (not (evenp (digitn (f (u_i from v)) (- from) (D))))))
-   :cases ((= (t_i from v) (expt (D) from)))
-   :enable (f e u_i w_i result-1-5 digitn-def ordD-t_i)
-   :disable evenp
-   :prep-lemmas
-   ((defrule lemma
-      (equal (evenp (mod (s_i i v) (D)))
-             (not (evenp (mod (t_i i v) (D)))))
-      :enable t_i))))
-
-#|
-(acl2::with-arith5-help
-(defruled algo1-meets-specs
    (mv-let (i dv) (algo1 from v f)
      (implies (and (in-tau-intervalp d (Rv v f))
                  (has-D-length d j)
@@ -229,73 +177,68 @@
                      (= (abs (- dv v)) (abs (- d v)))
                      (evenp (digitn (f dv) (- i) (D)))
                      (not (evenp (digitn (f d) (- j) (D))))))))
-; :cases ((< (mv-nth 0 (algo1 from v f)) (max from j))
-;         (> (mv-nth 0 (algo1 from v f)) (max from j))
-;         (= (mv-nth 0 (algo1 from v f)) (max from j)))
+   :enable (;algo1
+            choose-u_i-w_i
+                  u_i-linear w_i-linear)
+ :disable evenp
+ :do-not-induct t
+ :use (algo1-as-choose-u_i-w_i
+       (:instance uninteresting-other-than-u_i-w_i
+                  (i (mv-nth 0 (algo1 from v f)))
+                  )
+       (:instance has-D-length-monotone
+                  (x d)
+                  (i j)
+                  (j (mv-nth 0 (algo1 from v f))))
+       )))
+|#
+(acl2::with-arith5-help
+ (defrule evenp-digitn-f-w_i
+   (implies (and (integerp from)
+                 (<= 2 from))
+            (equal (evenp (digitn (f (w_i from v)) (- from) (D)))
+                   (not (evenp (digitn (f (u_i from v)) (- from) (D))))))
+   :cases ((= (t_i from v) (expt (D) from)))
+   :enable (f e u_i w_i result-1-5 digitn-def ordD-t_i)
+   :disable evenp
+   :prep-lemmas
+   ((defrule lemma
+      (equal (evenp (mod (s_i i v) (D)))
+             (not (evenp (mod (t_i i v) (D)))))
+      :enable t_i))))
+
+(acl2::with-arith5-help
+ (defruled algo1-meets-specs
+   (mv-let (i dv) (algo1 from v f)
+     (implies (and (in-tau-intervalp d (Rv v f))
+                 (has-D-length d j)
+                 (pos-rationalp d)
+                 (pos-rationalp v)
+                 (not (= d dv))
+                 (integerp from)
+                 (<= 2 from)
+                 (integerp j)
+                 (<= from j))
+            (or (< i j)
+                (and (= i j)
+                     (< (abs (- dv v)) (abs (- d v))))
+                (and (= i j)
+                     (= (abs (- dv v)) (abs (- d v)))
+                     (evenp (digitn (f dv) (- i) (D)))
+                     (not (evenp (digitn (f d) (- j) (D))))))))
  :induct (algo1 from v f)
  :enable (algo1 u_i-linear w_i-linear)
  :disable evenp
  :hints
- (("subgoal *1/7" :by lemma-1/7;:in-theory (enable in-tau-intervalp-Rv not-has-D-length-between-u_i-w_i)
-;   :use (:instance not-has-D-length-between-u_i-w_i (x d) (i from))
-  ; :use ((:instance uninteresting-other-than-u_i-w_i (i from)))
-   )
-  ("subgoal *1/6" :use ((:instance uninteresting-other-than-u_i-w_i (i from))))
-  ("subgoal *1/5" :use ((:instance uninteresting-other-than-u_i-w_i (i from))))
-  ("subgoal *1/4" :by lemma-1/4)
-  ("subgoal *1/3" :by lemma-1/3)
-  ("subgoal *1/2" :by lemma-1/2)
-  ("subgoal *1/1" :by lemma-1/1))))
-
- (("subgoal *1/7"
-   :use ((:instance uninteresting-other-than-u_i-w_i (i from))
-         (:instance has-D-length-monotone (x d) (i j) (j from))))
-  ("subgoal *1/7.63" :by lemma-1/7.63)
-  ("subgoal *1/7.55" :by lemma-1/7.55)
-  ("subgoal *1/6"
-   :use ((:instance uninteresting-other-than-u_i-w_i ( i from))
-         (:instance has-D-length-monotone (x d) (i j) (j from))))
-  ("subgoal *1/6.73" :by lemma-1/6.73)
-  ("subgoal *1/6.65" :by lemma-1/6.65)
-  ("subgoal *1/5"
-   :use ((:instance uninteresting-other-than-u_i-w_i ( i from))
-         (:instance has-D-length-monotone (x d) (i j) (j from))))
-  ("subgoal *1/4"
-   :use ((:instance uninteresting-other-than-u_i-w_i ( i from))
-         (:instance has-D-length-monotone (x d) (i j) (j from))))
-  ("subgoal *1/3"
-   :use ((:instance uninteresting-other-than-u_i-w_i ( i from))
-         (:instance has-D-length-monotone (x d) (i j) (j from))))
-  ("subgoal *1/2"
-   :use ((:instance uninteresting-other-than-u_i-w_i ( i from))
-         (:instance has-D-length-monotone (x d) (i j) (j from))))
-  ("subgoal *1/1"
-   :use  i<=max-from-j-algo1
-         )
-  ))))
- :prep-lemmas
- ((defruled lemma
-    (equal (evenp (mod (s_i i v) (D)))
-           (not (evenp (mod (t_i i v) (D)))))
-    :enable t_i
-    )
-
-  (defrule evenp-digitn-f-w_i
-    (equal
-     (evenp (digitn (f (w_i from v)) (- (acl2::pos-fix from)) (D)))
-     (not (evenp (digitn (f (u_i from v)) (- (acl2::pos-fix from)) (D)))))
-    :cases ((= (t_i from v) (expt (D) (acl2::pos-fix from))))
-    :enable (f e u_i w_i result-1-5 digitn-def ordD-t_i lemma)
-    :disable evenp
-    :hints (("subgoal 1" :cases ((zp from) (= from 1)))
-            ("subgoal 1.2''" :in-theory (enable acl2::pos-fix)
-             )
-            )
-
-    )
-  )
- ))
-|#
+ (("subgoal *1/2" :in-theory (enable choose-u_i-w_i)
+    :use (:instance uninteresting-other-than-u_i-w_i
+                    (i from)))
+  ("subgoal *1/1" :in-theory (enable choose-u_i-w_i)
+   :use
+   (algo1-as-choose-u_i-w_i
+    (:instance uninteresting-other-than-u_i-w_i
+               (i (mv-nth 0 (algo1 from v f)))
+               ))))))
 
 (defruled in-tau-intervalp-u_i-or-w_i-when-in-tau-intervalp-has-D-length
   (let ((Rv (Rv v f))
